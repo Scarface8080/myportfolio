6604ddf19b6f2605a8701efd041825e3d500113f
Hamza Shoukat <100609471+Scarface8080@users.noreply.github.com>
2024-05-12 00:38:26 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/3d_portfolio/",
-  build: { chunkSizeWarningLimit: 1600, }
+  build: { chunkSizeWarningLimit: 1600, },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
 })
