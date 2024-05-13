c85ad0b5a8b988790d71b3da3ee227f7b4686099
Hamza Shoukat <100609471+Scarface8080@users.noreply.github.com>
2024-05-13 17:42:54 +0500

    Update base vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/3d_portfolio/",
+  base: "https://Scarface8080.github.io/3d_portfolio/",
   build: { chunkSizeWarningLimit: 1600, },
   esbuild: {
     jsxInject: `import React from 'react'`,
